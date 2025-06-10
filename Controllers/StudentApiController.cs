3fe65f667556faa0507381afa1674fd6ba8f6b80
Buddhika Walpita19 <buddhisanje@gmail.com>
2025-06-10 20:40:47 +0530

    Add project files.

---
+++ Controllers/StudentApiController.cs
@@ -0,0 +1,169 @@
+﻿using Business_Logic_Layer.Models.Request.Interfaces;
+using Business_Logic_Layer.Services.AbstactClasses;
+using Business_Logic_Layer.Services.ConcreteClasses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace SoftoneStudentManagmentSystem.Controllers
+{
+
+    /*Developer:Buddhika Walpita
+      Date:2025.05.29
+      Description:Student API controller for managing student data
+    
+    */
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentApiController : ControllerBase
+    {
+        private readonly AStudentService stuService;
+     
+
+        #region Dependancy Injection
+        //Apply dependancy injection
+        public StudentApiController(AStudentService _stuService)
+        {
+            this.stuService = _stuService;
+           
+        }
+        #endregion
+
+        #region Get Student Data
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        // [ResponseCache(Duration = 60)] //Cache Resonse 60 seconds
+        public async Task<IActionResult> GetAllStudentsAsync()  //Get All Student Data
+        {
+           //try
+           // {
+                var result = await this.stuService.GetStudents();
+                if (result == null || result.Count == 0)
+                    return NotFound();
+                return Ok(result);
+            //}
+            //catch (Exception ex)
+            //{
+            //    return StatusCode(500, $"Internal server error: {ex.Message}");
+            //}
+        }
+
+
+        [HttpGet("{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ResponseCache(Duration = 60)]
+        public async Task<IActionResult> GetStudentsByKeyAsync(Guid id)  //Get student data by Id
+        {
+            //try
+            //{
+                var result = await this.stuService.GetStudentsbyId(id);
+                if (result == null || result.Count == 0)
+                    return NotFound();
+                return Ok(result);
+            //}
+            //catch (Exception ex)
+            //{
+            //    return StatusCode(500, $"Internal server error: {ex.Message}");
+            //}
+        }
+
+        [HttpGet("{id:guid}/{isImage:bool}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetStudentsImageData(Guid id,bool isImage)  //Get student data by Id
+        {
+            //try
+            //{
+                var result = await this.stuService.GetStudentsImageById(id);
+                if (result == null)
+                    return NotFound();
+                return Ok(result);
+            //}
+            //catch (Exception ex)
+            //{
+            //    return StatusCode(500, $"Internal server error: {ex.Message}");
+            //}
+        }
+
+        [HttpGet("search")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ResponseCache(Duration = 60)]
+        public async Task<IActionResult> GetStudentsBySearchTextAsync(string search)  //Get student data by Search Text
+        {
+            //try
+            //{
+                var result = await this.stuService.GetStudentsbyText(search);
+                if (result == null || result.Count == 0)
+                    return NotFound();
+                return Ok(result);
+            //}
+            //catch (Exception ex)
+            //{
+            //    return StatusCode(500, $"Internal server error: {ex.Message}");
+            //}
+        }
+
+        #endregion
+
+        #region Update Student Data
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+
+        public async Task<IActionResult> SaveStudentData([FromBody] StudentRequest stuRequest)  //Save Student data
+        {
+            //try
+            //{
+                if (stuRequest == null)
+                {
+                    return BadRequest("Student Data is empty");
+                }
+
+                var result = await this.stuService.SaveStudentData(stuRequest);
+                if (result <= 0)
+                {
+                    return NotFound("Student not found");
+                }
+                //return Ok("Student saved successfully");
+                return Ok(result);
+        
+            //}
+            //catch (Exception ex)
+            //{
+            //    return StatusCode(500, $"Internal server error: {ex.Message}");
+            //}
+        }
+
+
+        #endregion
+
+        #region Delete Student Data
+        [HttpDelete("{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+
+
+        public async Task<IActionResult>  DeleteStudentData(Guid id)  //Delete Student data
+        {
+            //try
+            //{
+                if (id == Guid.Empty)
+                {
+                    return BadRequest("Invalid student ID");
+                }
+
+                var result = await this.stuService.DeleteStudentData(id);
+                return Ok(result);
+            //}
+            //catch (Exception ex)
+            //{
+            //    return StatusCode(500, $"Internal server error: {ex.Message}");
+            //}
+        }
+        #endregion
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,81 @@
+using Business_Logic_Layer;
+using DAL;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.OpenApi.Models;
+using SoftoneStudentManagmentSystem;
+using SoftoneStudentManagmentSystem.Middleware;
+using System.Text.Json;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddTransient<GlobalExceptionHandlerMiddleware>();
+//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Softone Student Management System", Version = "v1" });
+});
+
+//Add dependancy injection for DAL and BLL layers
+builder.Services.RegisterDALDependencies(builder.Configuration); 
+builder.Services.RegisterBLLDependencies(builder.Configuration);
+//Upgrade to Http 1 and Http 2
+builder.WebHost.ConfigureKestrel(options =>
+{
+    // HTTP/1.1
+    options.ListenAnyIP(5000);
+
+    // HTTPS with HTTP/1.1, HTTP/2 and HTTP/3
+    options.ListenAnyIP(5001, listenOptions =>
+    {
+        listenOptions.UseHttps();
+        listenOptions.Protocols = HttpProtocols.Http1AndHttp2AndHttp3;
+    });
+});
+//Tune of Json Serialization
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+
+        options.JsonSerializerOptions.IgnoreNullValues = true;
+    });
+
+
+//var conString = builder.Configuration.GetConnectionString("StuConnStr") ??
+//     throw new InvalidOperationException("Connection string 'StuConnStr'" +
+//    " not found.");
+//builder.Services.AddDbContext<SofttOneSmsnewContext>(options =>
+//    options.UseSqlServer(conString));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseMiddleware<GlobalExceptionHandlerMiddleware>();
+app.UseAuthorization();
+
+// Enable CORS to allow requests from any origin, header, and method
+//Allow 3rd party frond end to connect this Api
+app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
+
+app.MapControllers();
+
+app.Run();
